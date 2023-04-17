b7c0093f811672f136b7374934c412a25c173c15
Sandra <62030769+berzinasandra@users.noreply.github.com>
2023-04-17 21:31:53 +0200

    Add files via upload

--- src/helpers/common_functions.py
+++ src/helpers/common_functions.py
@@ -1,10 +1,9 @@
 import requests
-from dotenv import load_dotenv # pip3 install python-dotenv
+from dotenv import load_dotenv  # pip3 install python-dotenv
 import logging
 import os
 from dataclasses import dataclass
 from datetime import datetime
-from helpers.generate_token import get_token
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("REQUESTS")
@@ -12,28 +11,28 @@ logger.setLevel(logging.INFO)
 
 load_dotenv()
 
+
 def _make_spotify_request(url, offset, spotify_token):
     token = spotify_token
-    header = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json'
-    }
-    import pdb;pdb.set_trace()
+    header = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
 
-    response = requests.get(url+str(offset), headers=header)
+    response = requests.get(url + str(offset), headers=header)
     return response
+
 
 def _request_endpoint(service, url, offset, spotify_token):
     if service == "spotify":
         response = _make_spotify_request(url, offset, spotify_token)
-    elif service == 'ticketmaster':
+    elif service == "ticketmaster":
         response = requests.get(url)
     return response
+
 
 def _retrieve_items(response, service):
     if 300 > response.status_code >= 200:
         items = response.json()
-        return items['items'] if service == 'spotify' else items
+        return items["items"] if service == "spotify" else items
+
 
 def _check_response(response, service, items):
     respose_good = False
@@ -43,13 +42,16 @@ def _check_response(response, service, items):
         logger.info(f"{service.title()} Token has expired, need to update it!")
         return False
     else:
-        print(f'FAILED {response.status_code} - {response.text}')
+        print(f"FAILED {response.status_code} - {response.text}")
 
-def make_request(url:str=None, offset:int=None, service:str=None, spotify_token:str=None):
+
+def make_request(
+    url: str = None, offset: int = None, service: str = None, spotify_token: str = None
+):
     response = _request_endpoint(service, url, offset, spotify_token)
     items = _retrieve_items(response, service)
-    
-    if not _check_response and service == 'spotify':
+
+    if not _check_response and service == "spotify":
         print("Create new token")
         response = _request_endpoint(service, url, offset, spotify_token)
         items = _retrieve_items(response, service)
@@ -74,11 +76,11 @@ class Event:
     ticket_type_name: str
     ticket_type_id: str
     ticket_url: list
-    concert_date:str
-    concert_starting_time:str
-    concert_timezone:str
+    concert_date: str
+    concert_starting_time: str
+    concert_timezone: str
     tikcet_status: str
     genre: list
     promoter_info: dict
-    venues :list
+    venues: list
     social_media_links: list
--- src/helpers/generate_token.py
+++ src/helpers/generate_token.py
@@ -1,26 +1,27 @@
 import requests
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-url = 'https://accounts.spotify.com/api/token'
-header = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
+url = "https://accounts.spotify.com/api/token"
+header = {"Content-Type": "application/x-www-form-urlencoded"}
 data = {
-    'grant_type': 'client_credentials',
-    'client_id': SPOTIFY_CLIENT_ID,
-    'client_secret': SPOTIFY_CLIENT_SECRET,
+    "grant_type": "client_credentials",
+    "client_id": SPOTIFY_CLIENT_ID,
+    "client_secret": SPOTIFY_CLIENT_SECRET,
 }
+
+
 def get_token():
     response = requests.post(url, headers=header, data=data)
     if 300 > response.status_code >= 200:
         token = response.json().get("access_token")
         return token
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_token()
